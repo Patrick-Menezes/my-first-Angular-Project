39054587ed6c43bf15d1c5e1e6ef98f12939a449
Patrick Lorran Almeida Menezes <patrickalmenezes@gmail.com>
2023-09-05 19:54:46 -0300

    adicionando estrutura

--- src/app/packages/packages-list/packages-list.component.ts
+++ src/app/packages/packages-list/packages-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Package } from './package.model';
 
 @Component({
   selector: 'app-packages-list',
@@ -7,7 +8,11 @@ import { Component } from '@angular/core';
 })
 export class PackagesListComponent {
 
-packages=[];
+packages:Package[]=[
+new Package('Churrasco','Churrasco de Picanha','https://th.bing.com/th/id/OIP.vAyXtbX1rGHQ6aP8PWoWTgHaFM?w=287&h=201&c=7&r=0&o=5&pid=1.7')
+
+
+];
 
 
 
