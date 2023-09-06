abc3605b0bb98e62f3c5b84606310041fb932429
Patrick Lorran Almeida Menezes <patrickalmenezes@gmail.com>
2023-09-06 18:13:57 -0300

    informações adicionadas

--- src/app/packages/packages-list/packages-list.component.ts
+++ src/app/packages/packages-list/packages-list.component.ts
@@ -9,6 +9,7 @@ import { Package } from './package.model';
 export class PackagesListComponent {
 
 packages:Package[]=[
+new Package('Churrasco','Churrasco de Picanha','https://th.bing.com/th/id/OIP.vAyXtbX1rGHQ6aP8PWoWTgHaFM?w=287&h=201&c=7&r=0&o=5&pid=1.7'),
 new Package('Churrasco','Churrasco de Picanha','https://th.bing.com/th/id/OIP.vAyXtbX1rGHQ6aP8PWoWTgHaFM?w=287&h=201&c=7&r=0&o=5&pid=1.7')
 
 
